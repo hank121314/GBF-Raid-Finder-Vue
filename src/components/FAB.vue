01835bfe218ad21f8bdf30b7a15f4d2dd9766a60
Hank Chen <hank121314@gmail.com>
2021-07-09 15:59:13 +0800

    feat(components/FAB): emit click

--- src/components/FAB.vue
+++ src/components/FAB.vue
@@ -1,28 +1,33 @@
 <template>
-  <div class="fixed right-6 bottom-6 w-12 h-12 flex justify-center items-center">
-    <button
-      v-wave
-      class="rounded-full focus:outline-none w-full h-full bg-gray-700 flex justify-center items-center hover:bg-gray-500"
-    >
-      <adjustments-icon class="h-8 w-8 text-white" />
-    </button>
-  </div>
+	<div
+		class="fixed right-6 bottom-6 w-12 h-12 flex justify-center items-center"
+	>
+		<button
+			v-wave
+			class="rounded-full focus:outline-none w-full h-full bg-gray-700 flex justify-center items-center hover:bg-gray-500"
+			@click="onClick"
+		>
+			<adjustments-icon class="h-8 w-8 text-white" />
+		</button>
+	</div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import { AdjustmentsIcon } from '@heroicons/vue/solid';
+import { defineComponent } from "vue"
+import { AdjustmentsIcon } from "@heroicons/vue/solid"
 
 export default defineComponent({
-  name: "FAB",
-  components: { AdjustmentsIcon },
-  setup() {
+	name: "FAB",
+	components: { AdjustmentsIcon },
+	emits: ["click"],
+	setup(_, { emit }) {
+		const onClick = () => {
+			emit("click")
+		}
 
-
-    return {}
-  }
+		return { onClick }
+	}
 })
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
