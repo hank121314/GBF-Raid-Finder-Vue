8ae8a8157f8238b7691b275e1ba6d08464253f27
Hank Chen <hank121314@gmail.com>
2021-07-09 15:52:38 +0800

    feat(heroicons): add some new icons

--- src/@types/heroicons.d.ts
+++ src/@types/heroicons.d.ts
@@ -1,3 +1,6 @@
 declare module "@heroicons/vue/solid" {
 	export { default as AdjustmentsIcon } from "@heroicons/vue/solid/esm/AdjustmentsIcon.js"
+	export { default as ChevronDownIcon } from "@headlessui/vue/solid/esm/ChevronDownIcon.js"
+	export { default as HeartIcon } from "@headlessui/vue/solid/esm/HeartIcon.js"
+
 }
