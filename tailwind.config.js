d7567a154774ab18d0bb9d63d56903258f9a9fd9
Hank Chen <hank121314@gmail.com>
2021-07-09 15:54:29 +0800

    feat(tailwind): add some color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,37 @@
+const colors = require("tailwindcss/colors")
+
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: 'class',
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {
-      backgroundColor: ['active'],
-    },
-  },
-  plugins: [],
-};
+	purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+	darkMode: "class",
+	theme: {
+		extend: {
+			colors: {
+				transparent: "transparent",
+				black: colors.black,
+				white: colors.white,
+				gray: colors.gray,
+				coolGray: "#4E4E4E",
+				darkGray: "#222222",
+				background: "#202128"
+			},
+			backgroundSize: {
+				"full": "100%"
+			},
+			backgroundPosition: {
+				"top-1/5": "0% 20%"
+			},
+			inset: {
+				"1/20": "5%"
+			},
+			maxWidth: {
+				"9/10": "90%"
+			}
+		}
+	},
+	variants: {
+		extend: {
+			backgroundColor: ["active"]
+		}
+	},
+	plugins: []
+}
