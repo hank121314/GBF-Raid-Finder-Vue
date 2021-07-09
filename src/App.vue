d991d38caa4941b5b7c196b4b05c5018ba017461
Hank Chen <hank121314@gmail.com>
2021-07-09 15:58:27 +0800

    feat(App): Toggle html body onStart and handle Settings status

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,36 @@
 <template>
-  <f-a-b />
+	<f-a-b @click="onSettingsOpen" />
+	<settings :is-open="isSettingsOpen" @close="onSettingsClose" />
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import { FAB } from '@/components'
+import { defineComponent, ref, onMounted } from "vue"
+import { FAB } from "@/components"
+import { Settings } from "@/pages"
 
 export default defineComponent({
-  name: 'App',
-  components: { FAB },
+	name: "App",
+	components: { FAB, Settings },
+	setup() {
+		onMounted(() => {
+			document.body.classList.add("bg-background")
+		})
+
+		const isSettingsOpen = ref(false)
+
+		const onSettingsOpen = () => {
+			isSettingsOpen.value = true
+		}
+
+		const onSettingsClose = () => {
+			isSettingsOpen.value = false
+		}
+
+		return {
+			isSettingsOpen,
+			onSettingsOpen,
+			onSettingsClose
+		}
+	}
 })
 </script>
