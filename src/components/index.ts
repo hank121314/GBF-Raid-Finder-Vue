b0f872e751056e1baf4df79121699cb5bfa2dd80
Hank Chen <hank121314@gmail.com>
2021-07-09 16:01:26 +0800

    feat(components): export BossTile, Tab and Selector

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,5 @@
+export { default as BossTile } from "./BossTile.vue"
 export { default as FAB } from "./FAB.vue"
+export { default as Tab } from "./Tab.vue"
+export { default as Selector } from "./Selector.vue"
+
