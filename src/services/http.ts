383eb25df1f814da6d5d0fd007fdc21574c10f9b
Hank Chen <hank121314@gmail.com>
2021-07-14 10:09:31 +0800

    feat(server/http): add get_persistence_boss

--- src/services/http.ts
+++ src/services/http.ts
@@ -1,19 +1,21 @@
 import axios, { AxiosInstance } from "axios"
 import { baseURL } from "@/resources/settings"
 import RaidBoss from "@/proto/raid_boss"
+import RaidTweet from "@/proto/raid_tweet"
 
 export type HttpClientInstance = {
 	get_bosses(level: number): Promise<RaidBoss[]>
+	get_persistence_boss(
+		boss_names: string[]
+	): Promise<Record<string, RaidTweet[]>>
 }
 
 class HttpClient implements HttpClientInstance {
-	#baseURL: string
-	#inner_client: AxiosInstance
+	#innerClient: AxiosInstance
 
 	constructor(baseURL: string) {
-		this.#baseURL = baseURL
-		this.#inner_client = axios.create({
-			baseURL: this.#baseURL,
+		this.#innerClient = axios.create({
+			baseURL: "https://" + baseURL,
 			responseType: "json"
 		})
 	}
@@ -27,13 +29,39 @@ class HttpClient implements HttpClientInstance {
 		const params = {
 			level
 		}
-		const response = await this.#inner_client.post<Uint8Array[]>(
+		const response = await this.#innerClient.post<Uint8Array[]>(
 			"/get_bosses",
 			params,
 			config
 		)
 		const { data } = response
 		return data.map((d) => new RaidBoss(d))
+	}
+
+	async get_persistence_boss(
+		boss_names: string[]
+	): Promise<Record<string, RaidTweet[]>> {
+		const config = {
+			headers: {
+				"Content-Type": "application/json"
+			}
+		}
+		const params = {
+			boss_names
+		}
+		const response = await this.#innerClient.post<Record<string, Uint8Array[]>>(
+			"/get_persistence_boss",
+			params,
+			config
+		)
+		const { data } = response
+		return Object.keys(data).reduce(
+			(memo, key) => ({
+				[key]: data[key].map((d) => new RaidTweet(d)),
+				...memo
+			}),
+			{}
+		)
 	}
 }
 
