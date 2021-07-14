cbe1eb4aa11b8345134d5d4b06e451f2ab3ca155
Hank Chen <hank121314@gmail.com>
2021-07-14 10:13:21 +0800

    resources(settings): add wsPath and Language enum

--- src/resources/settings.ts
+++ src/resources/settings.ts
@@ -1,8 +1,14 @@
-export const baseURL: Readonly<string> =
-	"https://api-gbf-raid-finder.amlispiration.com"
+export const baseURL: Readonly<string> = "api-gbf-raid-finder.amlispiration.com"
+
+export const wsPath = "stream_bosses"
 
 export enum TimeFormation {
 	Relative = "relative",
 	TwelveHour = "twelveHour",
 	TwentyFourHour = "twentyFourHour"
+}
+
+export enum Language {
+	EN = "en",
+	ZH_TW = "zh-TW"
 }
