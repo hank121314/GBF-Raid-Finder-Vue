2096679054e859727d0f391ec302392c76b0f96d
Hank Chen <hank121314@gmail.com>
2021-07-14 10:06:37 +0800

    feat(heroicons): add RefreshIcon

--- src/@types/heroicons.d.ts
+++ src/@types/heroicons.d.ts
@@ -1,6 +1,6 @@
 declare module "@heroicons/vue/solid" {
 	export { default as AdjustmentsIcon } from "@heroicons/vue/solid/esm/AdjustmentsIcon.js"
-	export { default as ChevronDownIcon } from "@headlessui/vue/solid/esm/ChevronDownIcon.js"
-	export { default as HeartIcon } from "@headlessui/vue/solid/esm/HeartIcon.js"
-
+	export { default as ChevronDownIcon } from "@heroicons/vue/solid/esm/ChevronDownIcon.js"
+	export { default as HeartIcon } from "@heroicons/vue/solid/esm/HeartIcon.js"
+	export { default as RefreshIcon } from "@heroicons/vue/solid/esm/RefreshIcon.js"
 }
