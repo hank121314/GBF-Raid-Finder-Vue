4298e71eabeb8609ee4c65c9b9b433d32f1dff59
Hank Chen <hank121314@gmail.com>
2021-07-15 18:23:58 +0800

    feat(heroicons): add some icons

--- src/@types/heroicons.d.ts
+++ src/@types/heroicons.d.ts
@@ -1,6 +1,16 @@
 declare module "@heroicons/vue/solid" {
 	export { default as AdjustmentsIcon } from "@heroicons/vue/solid/esm/AdjustmentsIcon.js"
 	export { default as ChevronDownIcon } from "@heroicons/vue/solid/esm/ChevronDownIcon.js"
+	export { default as ChevronRightIcon } from "@heroicons/vue/solid/esm/ChevronRightIcon.js"
+	export { default as ChevronDoubleDownIcon } from "@heroicons/vue/solid/esm/ChevronDoubleDownIcon.js"
+	export { default as ChevronLeftIcon } from "@heroicons/vue/solid/esm/ChevronLeftIcon.js"
 	export { default as HeartIcon } from "@heroicons/vue/solid/esm/HeartIcon.js"
+	export { default as MenuIcon } from "@heroicons/vue/solid/esm/MenuIcon.js"
+	export { default as MinusIcon } from "@heroicons/vue/solid/esm/MinusIcon.js"
 	export { default as RefreshIcon } from "@heroicons/vue/solid/esm/RefreshIcon.js"
+}
+
+declare module "@heroicons/vue/outline" {
+	export { default as ArrowsExpandIcon } from "@heroicons/vue/outline/esm/ArrowsExpandIcon.js"
+	export { default as BanIcon } from "@heroicons/vue/outline/esm/BanIcon.js"
 }
