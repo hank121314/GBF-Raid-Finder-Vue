e3c35bfef6c82d00abf86e21fb4a798b8c91a12c
Hank Chen <hank121314@gmail.com>
2021-07-28 16:53:30 +0800

    feat(components/FAB): add aria-label

--- src/components/FAB.vue
+++ src/components/FAB.vue
@@ -1,10 +1,19 @@
 <template>
-	<div
-		class="fixed right-6 bottom-6 w-12 h-12 flex justify-center items-center z-10"
-	>
+	<div class="fixed right-6 bottom-6 w-12 h-12 flex justify-center items-center z-10">
 		<button
 			v-wave
-			class="rounded-full focus:outline-none w-full h-full bg-gray-700 flex justify-center items-center hover:bg-gray-500"
+			aria-label="fab"
+			class="
+				rounded-full
+				focus:outline-none
+				w-full
+				h-full
+				bg-gray-700
+				flex
+				justify-center
+				items-center
+				hover:bg-gray-500
+			"
 			@click="onClick"
 		>
 			<adjustments-icon class="h-8 w-8 text-white" />
