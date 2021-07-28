0b2949407ca0025d306003de506065dfd848631a
Hank Chen <hank121314@gmail.com>
2021-07-28 16:23:11 +0800

    feat(@types): add vue-virtual-scroller type definition

---
+++ src/@types/vue-virtual-scroller.d.ts
@@ -0,0 +1,5 @@
+declare module "vue-virtual-scroller" {
+	import { Component } from "vue"
+	export const DynamicScroller: Component<any, any, any, any>
+	export const DynamicScrollerItem: Component<any, any, any, any>
+}
