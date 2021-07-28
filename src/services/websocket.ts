f7673007eb1287c4356b54a3397cc352e07ca437
Hank Chen <hank121314@gmail.com>
2021-07-28 16:28:34 +0800

    feat(services/websocket): implement custom ping/pong api

--- src/services/websocket.ts
+++ src/services/websocket.ts
@@ -1,6 +1,11 @@
 import mitt, { Emitter } from "mitt"
-import { baseURL, wsPath } from "@/resources/settings"
+import { WS_PATH, WS_PROTOCOL } from "@/resources/settings"
 import RaidTweet from "@/proto/raid_tweet"
+
+const KEEPALIVE = 5000
+
+const PING = "__PING__"
+const PONG = "__PONG__"
 
 export enum WebsocketEvents {
 	OPEN = "OPEN",
@@ -9,7 +14,7 @@ export enum WebsocketEvents {
 	MESSAGE = "MESSAGE"
 }
 
-type Event = {
+export type Event = {
 	kind: WebsocketEvents
 	state?: number
 	data?: RaidTweet
@@ -21,25 +26,36 @@ type Events = {
 
 type onEvent = (e: Event) => void
 
-class WebsocketClient {
+export default class WebsocketClient {
 	#baseURL: string
 	#innerClient?: WebSocket | null
 	#emitter: Emitter<Events>
-	#followed: string[]
+	#lossPacket: number[]
+	#keepAliveTimer?: number
 
 	constructor(baseURL: string) {
 		this.#baseURL = baseURL
 		this.#emitter = mitt()
-		this.#followed = []
+		this.#lossPacket = []
 	}
 
+	/**
+	 * Connect to websocket server.
+	 * if this.#innerClient is exist means it is already connected.
+	 * Will start keepAlive once client is connected.
+	 * And mapping the websocket events into mitt events.
+	 * @returns {Promise<void>}
+	 */
 	async connect(): Promise<void> {
-		this.#innerClient = new WebSocket("wss://" + this.#baseURL + "/" + wsPath)
+		if (this.#innerClient) return
+		this.#innerClient = new WebSocket(WS_PROTOCOL + this.#baseURL + "/" + WS_PATH)
+		this.#innerClient.binaryType = "arraybuffer"
 		return new Promise<void>((resolve, reject) => {
 			if (this.#innerClient) {
 				this.#innerClient.onopen = () => {
-					if (this.#innerClient?.readyState === 1) {
+					if (this.#innerClient?.readyState === WebSocket.OPEN) {
 						this.onInnerConnect()
+						this.keepAlive()
 						resolve()
 					}
 				}
@@ -59,52 +75,104 @@ class WebsocketClient {
 				this.#innerClient.onclose = () => {
 					this.onInnerClose()
 				}
-				this.#innerClient.onmessage = async (e: MessageEvent<Blob>) => {
-					const buffer = await e.data.arrayBuffer()
-					this.onInnerMessage(new Uint8Array(buffer))
-				}
-			}
-		})
-	}
-
-	async close(): Promise<void> {
-		return new Promise((resolve) => {
-			if (this.#innerClient) {
-				this.#innerClient.onclose = () => {
-					if (this.#innerClient?.readyState === WebSocket.CLOSED) {
-						resolve()
+				this.#innerClient.onmessage = (e: MessageEvent<Uint8Array | string>) => {
+					if (e.data === PONG) {
+						this.#lossPacket.pop()
+					} else {
+						this.onInnerMessage(new Uint8Array(e.data as ArrayBufferLike))
 					}
 				}
-				this.#innerClient.close()
-				this.#innerClient = null
 			}
 		})
 	}
 
-	registerFollowBosses(followed: string[]) {
-		this.#followed = followed
-		this.#innerClient?.send(JSON.stringify({ boss_names: this.#followed }))
+	/**
+	 * Close websocket client and destroy the this.#innerClient instance
+	 *
+	 * Note: Since onclose timeout is a little bit longer, we do not wait until onclose is triggered
+	 */
+	async close(): Promise<void> {
+		if (this.#innerClient) {
+			this.#innerClient.close()
+			// Unsubscribe the event listener
+			this.#innerClient.onopen = null
+			this.#innerClient.onmessage = null
+			this.#innerClient.onclose = null
+			this.#innerClient.onerror = null
+		}
+		// Destroy the instance
+		this.#innerClient = null
+		this.#lossPacket = []
+		clearInterval(this.#keepAliveTimer)
 	}
 
-	subscribeEvent(callback: onEvent) {
+	/**
+	 * Reconnect websocket client
+	 * @returns {Promise<void>}
+	 */
+	async reconnect(): Promise<void> {
+		return (
+			this.close()
+				.then(this.connect.bind(this))
+				// eslint-disable-next-line @typescript-eslint/no-empty-function
+				.catch(this.close.bind(this))
+		)
+	}
+
+	keepAlive(): Promise<void> | void {
+		// If this.#innerClient instance is undefined(null) call reconnect directly.
+		if (!this.#innerClient)
+			return this.reconnect().finally(() => {
+				this.#keepAliveTimer = setTimeout(this.keepAlive.bind(this), KEEPALIVE)
+			})
+		// After sending PING packet add zero to this.#lossPacket.
+		// this.#lossPacket will pop while receiving PONG packet.
+		// If this.#lossPacket length is greater than one that means we sent one PING packets
+		// with no PONG response(this.#lossPacket did not pop).
+		// This situation indicate that the connection might be unstable.
+		// So we will have to reconnect when this.#lossPacket is greater than one.
+		this.#innerClient.send(PING)
+		if (this.#lossPacket.length <= 1) {
+			this.#lossPacket.push(0)
+		} else {
+			return this.reconnect().finally(() => {
+				this.#keepAliveTimer = setTimeout(this.keepAlive.bind(this), KEEPALIVE)
+			})
+		}
+		this.#keepAliveTimer = setTimeout(this.keepAlive.bind(this), KEEPALIVE)
+	}
+
+	/**
+	 * Send the list of names to websocket server.
+	 * @param {string[]} followed
+	 */
+	registerFollowBosses(followed: string[]): void {
+		this.#innerClient?.send(JSON.stringify({ boss_names: followed }))
+	}
+
+	/**
+	 * Subscribe the event from websocket client
+	 * @param {onEvent} callback
+	 */
+	subscribeEvent(callback: onEvent): void {
 		this.#emitter.on("event", callback)
 	}
 
-	onInnerConnect() {
+	onInnerConnect(): void {
 		this.#emitter.emit("event", {
 			kind: WebsocketEvents.OPEN,
 			state: this.#innerClient?.readyState
 		})
 	}
 
-	onInnerClose() {
+	onInnerClose(): void {
 		this.#emitter.emit("event", {
 			kind: WebsocketEvents.CLOSE,
 			state: this.#innerClient?.readyState
 		})
 	}
 
-	onInnerMessage(input: Uint8Array) {
+	onInnerMessage(input: Uint8Array): void {
 		this.#emitter.emit("event", {
 			kind: WebsocketEvents.MESSAGE,
 			state: this.#innerClient?.readyState,
@@ -112,12 +180,10 @@ class WebsocketClient {
 		})
 	}
 
-	onInnerError() {
+	onInnerError(): void {
 		this.#emitter.emit("event", {
 			kind: WebsocketEvents.ERROR,
 			state: this.#innerClient?.readyState
 		})
 	}
 }
-
-export default new WebsocketClient(baseURL)
