aaec1a832e388d4b202c247ca87d35b93d437168
Hank Chen <hank121314@gmail.com>
2021-07-28 16:29:27 +0800

    feat(resources/settings): add some settings

--- src/resources/settings.ts
+++ src/resources/settings.ts
@@ -1,3 +1,7 @@
-export const baseURL: Readonly<string> = "api-gbf-raid-finder.amlispiration.com"
+export const BASE_URL: Readonly<string> = "api-gbf-raid-finder.amlispiration.com"
 
-export const wsPath = "stream_bosses"
+export const HTTP_PROTOCOL = "https://"
+
+export const WS_PROTOCOL = "wss://"
+
+export const WS_PATH = "stream_bosses"
