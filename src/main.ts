2804adbc0a12a8a913ffad9962c90d637ce65bf7
Hank Chen <hank121314@gmail.com>
2021-07-28 16:32:38 +0800

    feat(main): remove some workaround

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,11 @@
 import { createApp } from "vue"
 import VWave from "v-wave"
-import { util, configure } from "protobufjs/minimal"
-import * as Long from "long"
 import App from "@/App.vue"
 import i18n from "@/locales"
 import store, { key } from "@/store"
 import "@/locales/dayjs"
 import "mosha-vue-toastify/dist/style.css"
+import 'vue-virtual-scroller/dist/vue-virtual-scroller.css'
 import "./index.css"
-
-// Fixes: https://github.com/protobufjs/protobuf.js/issues/940
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-util.Long = Long
-configure()
 
 createApp(App).use(store, key).use(i18n).use(VWave).mount("#app")
