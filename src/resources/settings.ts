803d405000aa1074df448bba341f0313aaa9fb4a
hank121314 <hank121314@gmail.com>
2021-08-03 18:05:48 +0800

    feat(resoures/settings): change all settings type to Readonly<string>

--- src/resources/settings.ts
+++ src/resources/settings.ts
@@ -1,7 +1,7 @@
 export const BASE_URL: Readonly<string> = "api-gbf-raid-finder.amlispiration.com"
 
-export const HTTP_PROTOCOL = "https://"
+export const HTTP_PROTOCOL: Readonly<string> = "https://"
 
-export const WS_PROTOCOL = "wss://"
+export const WS_PROTOCOL: Readonly<string> = "wss://"
 
-export const WS_PATH = "stream_bosses"
+export const WS_PATH: Readonly<string> = "stream_bosses"
