74331f556f9e64dbc5c0951ef9a26ede9a90c510
hank121314 <hank121314@gmail.com>
2021-08-03 17:34:21 +0800

    feat(pages): define TweetLists as AsyncComponent

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -3,4 +3,4 @@ import { defineAsyncComponent } from "vue"
 export const Settings = defineAsyncComponent({ loader: () => import("./Settings.vue") })
 export const BossList = defineAsyncComponent({ loader: () => import("./BossList.vue") })
 export const Environment = defineAsyncComponent({ loader: () => import("./Environment.vue") })
-export { default as TweetLists } from "./TweetLists/TweetLists.vue"
+export const TweetLists = defineAsyncComponent({ loader: () => import("./TweetLists/TweetLists.vue") })
