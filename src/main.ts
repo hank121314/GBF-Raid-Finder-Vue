ccaef928b792cdc3273d03dfb75f0fc66214fddf
hank121314 <hank121314@gmail.com>
2021-08-03 15:36:21 +0800

    deps(perfect-scroller): add

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import i18n from "@/locales"
 import store, { key } from "@/store"
 import "@/locales/dayjs"
 import "mosha-vue-toastify/dist/style.css"
+import 'perfect-scrollbar/css/perfect-scrollbar.css'
 import "./index.css"
 
 createApp(App).use(store, key).use(i18n).use(VWave).mount("#app")
