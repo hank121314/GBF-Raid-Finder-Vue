32578f8522395f970e44046e1e5602bebed2460f
hank121314 <hank121314@gmail.com>
2021-08-12 18:06:06 +0800

    fix(entrypoint): do not apply API_HOST_NAME when it is empty

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-ROOT_DIR=/usr/share/nginx/html
-echo "Applying env constants to production JS."
-for file in $ROOT_DIR/assets/*.*.js $ROOT_DIR/index.html
-do
-  echo "Processing $file ...";
+if [[ -z "${API_HOST_NAME}" ]]; then
+  echo "API_HOST_NAME seems to be undefined"
+else
+  ROOT_DIR=/usr/share/nginx/html
+  echo "Applying env constants to production JS."
+  for file in $ROOT_DIR/assets/*.*.js $ROOT_DIR/index.html
+  do
+    echo "Processing $file ...";
 
-  sed -i 's|API_HOST_NAME|'${API_HOST_NAME}'|g' $file
-done
+    sed -i 's|API_HOST_NAME|'${API_HOST_NAME}'|g' $file
+  done
+fi
