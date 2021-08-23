2b9755f00d2508b2df1943d6d614d0537e7c93bb
hank121314 <hank121314@gmail.com>
2021-08-23 14:20:59 +0800

    fix(settings): BASE_URL should depend on environment

--- src/resources/settings.ts
+++ src/resources/settings.ts
@@ -1,4 +1,4 @@
-export const BASE_URL: Readonly<string> = "API_HOST_NAME"
+export const BASE_URL: Readonly<string> = import.meta.env.DEV ? "api-gbf-raid-finder.amlispiration.com" :"API_HOST_NAME"
 
 export const HTTP_PROTOCOL: Readonly<string> = "https://"
 
